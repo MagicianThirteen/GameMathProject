9079714881d474214f7e1bbf14b10f60213f36a7
MagicianThirteen <MagicianThirteen@gmail.com>
2022-05-09 21:09:00 +0800

    一个点始终指向另一个点atob

--- AboutGameMath/Assets/Vector2D/Vector2DThing.cs
+++ AboutGameMath/Assets/Vector2D/Vector2DThing.cs
@@ -14,12 +14,19 @@ public class Vector2DThing : MonoBehaviour
     {
         Vector2 a = atf.position;
         Vector2 b = btf.position;
-        Gizmos.DrawLine(a,b);
-        Vector2 pt = transform.position;
-        float ptLength = pt.magnitude;
+        //Gizmos.DrawLine(a,b);
+        //a到b的方向
+        Vector2 atob = b - a;
+        Vector2 atobDir = atob.normalized;
+        Gizmos.DrawLine(a,a+atobDir);
+        //Gizmos.DrawLine(Vector2.zero+a,atobDir+a);
+        //Gizmos.DrawLine(a,atob);
+
+        /*Vector2 pt = transform.position;
+        float ptLength = pt.magnitude;*/
         //Distance
-        abDistance=Vector2.Distance(a, b);
+        /*abDistance=Vector2.Distance(a, b);
         Vector2 dirToPt = pt.normalized;
-        Gizmos.DrawLine(Vector2.zero, dirToPt);
+        Gizmos.DrawLine(Vector2.zero, dirToPt);*/
     }
 }
